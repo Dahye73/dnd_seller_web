ccc38f3a8488408fca224d94fa34591943aa33f0
최기환 <rlghks9806@gmail.com>
2023-07-07 20:00:13 +0900

    로그인 페이지 완성

--- src/components/LoginPage/loginForm/loginForm.jsx
+++ src/components/LoginPage/loginForm/loginForm.jsx
@@ -1,15 +1,42 @@
+import styles from "./loginForm.module.scss";
+import google from "../../../../public/images/google.png";
+import kakao from "../../../../public/images/kakao-talk.png";
+
 const LoginForm = () => {
   return (
-    <form>
+    <form className={styles.form}>
       <label>Login</label>
-      <input></input>
-      <input></input>
-      <div>
-        <div />
+      <input placeholder="id"></input>
+      <input
+        placeholder="password"
+        className={styles["input-no-margin"]}
+      ></input>
+      <p className={styles["text-right-align"]}>Forgot Password?</p>
+      <div className={styles.line}>
+        <hr />
         <p>OR</p>
-        <div />
+        <hr />
       </div>
-      <button>login</button>
+      <button className={styles["button-with-image"]}>
+        <img
+          src={google}
+          alt="google login"
+        />
+        continue with google
+      </button>
+      <button className={styles["button-with-image"]}>
+        <img
+          src={kakao}
+          alt="kakao login"
+        />
+        continue with kakao
+      </button>
+      <button
+        className={styles.loginBtn}
+        type="submit"
+      >
+        login
+      </button>
     </form>
   );
 };
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,16 @@
 import LoginForm from "../components/LoginPage/loginForm/loginForm";
+import SideImage from "../components/LoginPage/sideImage/sideImage";
+import styles from "./LoginPage.module.css";
 
 const LoginPage = () => {
   return (
-    <section>
-      <LoginForm />
-      <div>image here.</div>
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <LoginForm />
+      </div>
+      <div className={styles.image}>
+        <SideImage />
+      </div>
     </section>
   );
 };
