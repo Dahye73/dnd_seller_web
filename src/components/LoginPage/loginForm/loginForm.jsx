40c1b2d0e438e193acdbb5a7a00af5295cd70fd6
최기환 <rlghks9806@gmail.com>
2023-07-07 16:04:26 +0900

    사이드 네비게이션바 작업 완료

---
+++ src/components/LoginPage/loginForm/loginForm.jsx
@@ -0,0 +1,17 @@
+const LoginForm = () => {
+  return (
+    <form>
+      <label>Login</label>
+      <input></input>
+      <input></input>
+      <div>
+        <div />
+        <p>OR</p>
+        <div />
+      </div>
+      <button>login</button>
+    </form>
+  );
+};
+
+export default LoginForm;
