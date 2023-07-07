9741aba2fb65160b62530b7bd5b94a0caabfd1da
gijin <rkrhdlf1546@naver.com>
2023-07-07 16:05:54 +0900

    날짜별 갤린더, 총주문 박스, 총주문 그래프 작업

--- src/components/Statics/Statics_Date/Date_Title.jsx
+++ src/components/Statics/Statics_Date/Date_Title.jsx
@@ -1,37 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Date_Title.module.css';
 import IconButton from '@mui/material/IconButton';
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const Date_Title_go = () => {
-  return (
-    <div>
-      <div className={styles.today}>
-        <div className={styles.heading}> 
-          <h1>Today</h1>
-        </div>
-        <IconButton className={styles.icon}>  
-          <CalendarTodayIcon fontSize="large" />
-        </IconButton>  
-      </div>
+    const [selectedDate, setSelectedDate] = useState(new Date());
+    const [isOpen, setIsOpen] = useState(false);
 
-      <div className={styles.boxContainer}> {/* 클래스명 변경 */}
-        <div className={styles.box}>
-          <div className={styles.heading}> 
-            <h2 className={styles.title}>총 주문수</h2>
-          </div>
-          <p className={styles.number}>500</p>
-        </div>
+    const isToday = (date) => {
+        const today = new Date();
+        return date.getDate() === today.getDate() &&
+            date.getMonth() === today.getMonth() &&
+            date.getFullYear() === today.getFullYear();
+    }
 
-        <div className={styles.box}>
-          <div className={styles.heading}> 
-            <h2 className={styles.title}>총 금액</h2>
-          </div>
-          <p className={styles.number}>500</p>
+    const pad = (number) => {
+        return String(number).padStart(2, '0');
+    }
+
+    const formatDate = (date) => {
+        if (isToday(date)) {
+            return 'Today';
+        } else {
+            return `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())}`;
+        }
+    };
+
+    const handleOpen = () => {
+        setIsOpen(prev => !prev);
+    };
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.today}>
+                <div className={styles.heading}>
+                    <h2>{formatDate(selectedDate)}</h2>
+                </div>
+                <div className={styles.datePickerContainer}>
+                    <IconButton className={styles.icon} onClick={handleOpen}>
+                        {isOpen && (
+                            <DatePicker
+                                selected={selectedDate}
+                                onChange={date => setSelectedDate(date)}
+                                inline
+                            />
+                        )}
+                        <CalendarTodayIcon fontSize="large" />
+                    </IconButton>
+                </div>
+            </div>
+
+            <div className={styles.boxContainer}>
+                <div className={styles.box}>
+                    <div className={styles.heading}>
+                        <h2 className={styles.title}>총 주문수</h2>
+                    </div>
+                    <p className={styles.number}>500</p>
+                </div>
+
+                <div className={styles.box}>
+                    <div className={styles.heading}>
+                        <h2 className={styles.title}>총 금액</h2>
+                    </div>
+                    <p className={styles.number}>500</p>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Date_Title_go;
--- src/pages/StaticsPage.jsx
+++ src/pages/StaticsPage.jsx
@@ -1,8 +1,15 @@
 import Date_Title_go from "../components/Statics/Statics_Date/Date_Title";
+import Sales_Graph_go from "../components/Statics/Statics_Graph/Sales_Graph";
+import styles from "./StaticsPage.module.css";
 const StaticsPage = () => {
   return (
+    <div className={styles.container}>
     <div>
-    <Date_Title_go />
+      <Date_Title_go />
+    </div>
+    <div>
+      <Sales_Graph_go />
+    </div>
     </div>
   );
 };
