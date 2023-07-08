f27d0f388d89e509b673883ee6b22e8004ac3ffa
최기환 <rlghks9806@gmail.com>
2023-07-08 22:41:06 +0900

    메인페이지 작업 중 grid-item 3번째꺼 작업해야함

---
+++ src/components/StartPage/StartPage_logo/StartPage_logo.jsx
@@ -0,0 +1,7 @@
+import styles from "./StartPage_logo.module.scss";
+
+const StartPageLogo = () => {
+  return <h1 className={styles.logo}>DnD</h1>;
+};
+
+export default StartPageLogo;
--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,8 +1,44 @@
+import Card from "../UI/card";
+import StartPageLogo from "../components/StartPage/StartPage_logo/StartPage_logo";
+import StartPageStatics from "../components/StartPage/StartPage_statics/StartPage_statics";
+import styles from "./StartPage.module.scss";
+
 const StartPage = () => {
   return (
-    <div>
-      <h1>start page.</h1>
-    </div>
+    <section className={styles.container}>
+      <StartPageLogo />
+      <div className={styles["container-grid"]}>
+        <div className={styles["grid-item"]}>
+          <Card>
+            <StartPageStatics
+              title={"이번달 매출액"}
+              amount={1838900}
+              description={
+                <>
+                  매월 평균 대비 <strong className={"text-strong"}>21%</strong>
+                  증가했습니다.
+                </>
+              }
+            />
+          </Card>
+        </div>
+        <div className={styles["grid-item"]}>
+          <Card>
+            <StartPageStatics
+              title={"금일 매출액"}
+              amount={0}
+              description={<>판매를 시작해 주세요.</>}
+            />
+          </Card>
+        </div>
+        <div className={styles["grid-item"]}>
+          <Card>item3</Card>
+        </div>
+        <div className={styles["grid-item"]}>
+          <Card>item4</Card>
+        </div>
+      </div>
+    </section>
   );
 };
 
