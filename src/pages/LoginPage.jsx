a5de936f720f66b7eac13f65f4194562503b0a1f
최기환 <rlghks9806@gmail.com>
2023-07-08 16:37:36 +0900

    로그인 페이지 로그인 로직 구현 완료

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,19 @@
+import { useEffect } from "react";
 import LoginForm from "../components/LoginPage/loginForm/loginForm";
 import SideImage from "../components/LoginPage/sideImage/sideImage";
 import styles from "./LoginPage.module.css";
+import { useNavigate } from "react-router-dom";
+import { getCookie } from "../utilities/cookies";
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (getCookie("jwt")) {
+      navigate("/main");
+    }
+  }, [navigate]);
+
   return (
     <section className={styles.container}>
       <div className={styles.content}>
