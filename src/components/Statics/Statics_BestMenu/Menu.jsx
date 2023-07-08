0b88d3045b396d25bfa30adeff7312f623e42d52
gijin <rkrhdlf1546@naver.com>
2023-07-08 13:37:53 +0900

    기진 메인 가져오기

--- src/components/Statics/Statics_BestMenu/Menu.jsx
+++ src/components/Statics/Statics_BestMenu/Menu.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styles from './menu.module.css';
 import Box from '@mui/material/Box';
 import AttachMoney from '@mui/icons-material/AttachMoney';
@@ -7,18 +7,45 @@ import Favorite from '@mui/icons-material/Favorite';
 const Menu_go = () => {
     const [menus, setMenus] = useState([
         { id: 1, name: '메뉴1', price: 6000, favorite: 353, image: '/images/food1.jpg' },
-        { id: 2, name: '메뉴2', price: 6000, favorite: 353,image: '/images/food1.jpg' },
-        { id: 3, name: '메뉴3', price: 6000, favorite: 353,image: '/images/food1.jpg' },
-        { id: 4, name: '메뉴4', price: 6000, favorite: 353,image: '/images/food1.jpg' },
-
+        { id: 2, name: '메뉴2', price: 6000, favorite: 353, image: '/images/food1.jpg' },
+        { id: 3, name: '메뉴3', price: 6000, favorite: 353, image: '/images/food1.jpg' },
+        { id: 4, name: '메뉴4', price: 6000, favorite: 353, image: '/images/food1.jpg' },
+        { id: 5, name: '메뉴5', price: 6000, favorite: 353, image: '/images/food1.jpg' },
+        { id: 6, name: '메뉴6', price: 6000, favorite: 353, image: '/images/food1.jpg' },
+        { id: 7, name: '메뉴7', price: 6000, favorite: 353, image: '/images/food1.jpg' },
     ]);
+
+    const menuContainerRef = useRef();
+    const [isMouseDown, setIsMouseDown] = useState(false);
+
+    useEffect(() => {
+        let scrollInterval;
+        if (!isMouseDown) {
+            scrollInterval = setInterval(() => {
+                if (menuContainerRef.current) {
+                    if (menuContainerRef.current.scrollTop + menuContainerRef.current.clientHeight >= menuContainerRef.current.scrollHeight) {
+                        setTimeout(() => menuContainerRef.current.scrollTo(0, 0), 500);
+                    } else {
+                        menuContainerRef.current.scrollBy(0, 3);
+                    }
+                }
+            }, 50); 
+        }
+
+        return () => {
+            clearInterval(scrollInterval);
+        }
+    }, [isMouseDown]); 
 
     return (
         <div className={styles.container}>
             <div className={styles.titleContainer}>
                 <h2 className={styles.title}>인기 메뉴</h2>
             </div>
-            <div className={styles.menuContainer}>
+            <div className={styles.menuContainer} 
+                 ref={menuContainerRef}
+                 onMouseDown={() => setIsMouseDown(true)}
+                 onMouseUp={() => setIsMouseDown(false)}>
                 {menus.map((menu) => (
                     <Box key={menu.id} className={styles.box}>
                         <img className={styles.image} src={menu.image} alt={menu.name} />
@@ -30,7 +57,7 @@ const Menu_go = () => {
                             </div>
                         </div>
                         <div className={styles.priceContainer}>
-                            <AttachMoney fontSize="large"/>
+                            <AttachMoney fontSize="large" />
                             <span className={styles.menu_price}>{menu.price}</span>
                         </div>
                     </Box>
