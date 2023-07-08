9740e7f25719bdc1a9d0114fd6bc4a43f7f667ba
최기환 <rlghks9806@gmail.com>
2023-07-08 17:02:30 +0900

    로그인 안된 상태에서 다른 페이지 접속시 로그인페이지로 리디렉션 작업 완료

--- src/pages/MainRootLayout.jsx
+++ src/pages/MainRootLayout.jsx
@@ -1,8 +1,19 @@
 import { Outlet } from "react-router";
 import Nav from "../components/MainRootLayout/nav/nav.layout";
 import styles from "./MainRootLayout.module.css";
+import { useEffect } from "react";
+import { getCookie } from "../utilities/cookies";
+import { useNavigate } from "react-router-dom";
 
 const MainRootLayout = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!getCookie("jwt")) {
+      navigate("/");
+    }
+  }, [navigate]);
+
   return (
     <main className={styles.main}>
       <Nav />
