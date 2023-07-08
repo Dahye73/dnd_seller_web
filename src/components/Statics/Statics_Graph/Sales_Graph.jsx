1ffe974eb7ae0dd96d0e9baf624033acf74e6f63
gijin <rkrhdlf1546@naver.com>
2023-07-08 13:42:21 +0900

    1

--- src/components/Statics/Statics_Graph/Sales_Graph.jsx
+++ src/components/Statics/Statics_Graph/Sales_Graph.jsx
@@ -1,43 +1,47 @@
-import styles from "./Sales_Graph.module.css";
-import Box from "@mui/material/Box";
+import React from 'react';
+import styles from './Sales_Graph.module.css';
+import Box from '@mui/material/Box';
+import { ResponsiveBar } from '@nivo/bar';
 
 const Sales_Graph_go = () => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.titleContainer}>
-        <h2 className={styles.title}>매출 그래프</h2>
-        <select className={styles.selectBox}>
-          <option value="year">Year</option>
-          <option value="month">Month</option>
-          <option value="date">Date</option>
-        </select>
+    return (
+      <div className={styles.container}>
+        <div className={styles.titleContainer}>
+          <h2 className={styles.title}>매출 그래프</h2>
+          <select className={styles.selectBox}>
+            <option value="year">Year</option>
+            <option value="month">Month</option>
+            <option value="date">Date</option>
+          </select>
+        </div>
+  
+        <div className={styles.graphContainer}>
+          <Box 
+            className={styles.box}
+          >
+            <h3 className={styles.graphPlaceholder}>이곳에 그래프를 넣을 수 있습니다.</h3>
+          </Box>
+        </div>
+
+        <div className={styles.titleContainer}>
+          <h2 className={styles.title}>주문량 그래프</h2>
+          <select className={styles.selectBox}>
+            <option value="주문수">주문수</option>
+            <option value="찜수">찜수</option>
+          </select>
+        </div>
+  
+        <div className={styles.graphContainer}>
+          <Box 
+            className={styles.box}
+          >
+            <h3 className={styles.graphPlaceholder}>이곳에 그래프를 넣을 수 있습니다.</h3>
+          </Box>
+        </div>
       </div>
 
-      <div className={styles.graphContainer}>
-        <Box className={styles.box}>
-          <h3 className={styles.graphPlaceholder}>
-            이곳에 그래프를 넣을 수 있습니다.
-          </h3>
-        </Box>
-      </div>
-
-      <div className={styles.titleContainer}>
-        <h2 className={styles.title}>주문량 그래프</h2>
-        <select className={styles.selectBox}>
-          <option value="주문수">주문수</option>
-          <option value="찜수">찜수</option>
-        </select>
-      </div>
-
-      <div className={styles.graphContainer}>
-        <Box className={styles.box}>
-          <h3 className={styles.graphPlaceholder}>
-            이곳에 그래프를 넣을 수 있습니다.
-          </h3>
-        </Box>
-      </div>
-    </div>
-  );
-};
-
+      
+    );
+  };
+  
 export default Sales_Graph_go;
